8b93a6382c65e734a2052cd25394a26e02bb40e1
liimaleandro <63323900+liimaleandro@users.noreply.github.com>
2020-04-07 21:33:21 -0300

    added comment to explain
    code is efficient, but confusing

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    // tem que ter os + antes das variaveis
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
